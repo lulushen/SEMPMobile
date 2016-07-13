54dee2283632d9863202474538145ca01f6d0581
lulushen <592321785@qq.com>
2016-07-13 13:30:03 +0800

    安装环境cocoa pods

---
+++ SEMPMobile/SEMPMobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SEMPMobile
+//
+//  Created by 上海数聚 on 16/7/13.
+//  Copyright © 2016年 上海数聚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SEMPMobile/SEMPMobile/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SEMPMobile
+//
+//  Created by 上海数聚 on 16/7/13.
+//  Copyright © 2016年 上海数聚. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
