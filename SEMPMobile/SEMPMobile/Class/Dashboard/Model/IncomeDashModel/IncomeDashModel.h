dd7798df09dbecc9e6ec8630d40fbc4161bdf51b
lulushen <592321785@qq.com>
2016-08-18 18:19:26 +0800

    12

---
+++ SEMPMobile/SEMPMobile/Class/Dashboard/Model/IncomeDashModel/IncomeDashModel.h
@@ -0,0 +1,13 @@
+//
+//  IncomeDashModel.h
+//  SEMPMobile
+//
+//  Created by 上海数聚 on 16/8/18.
+//  Copyright © 2016年 上海数聚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IncomeDashModel : NSObject
+
+@end
---
+++ SEMPMobile/SEMPMobile/Class/Dashboard/Model/IncomeDashModel/IncomeDashModel.m
@@ -0,0 +1,13 @@
+//
+//  IncomeDashModel.m
+//  SEMPMobile
+//
+//  Created by 上海数聚 on 16/8/18.
+//  Copyright © 2016年 上海数聚. All rights reserved.
+//
+
+#import "IncomeDashModel.h"
+
+@implementation IncomeDashModel
+
+@end
